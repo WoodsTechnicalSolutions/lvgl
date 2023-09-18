8cdecb57e34da833a5729cae8dcde341e72146c9
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2023-09-18 12:48:21 +0200

    chart experiments

--- src/widgets/scale/lv_scale.h
+++ src/widgets/scale/lv_scale.h
@@ -173,7 +173,7 @@ void lv_scale_set_round_props(lv_obj_t * obj, uint16_t angle_range, int16_t rota
  * @param obj       pointer to a scale object
  * @param txt_src   pointer to an array of strings which will be display at major ticks
  */
-void lv_scale_set_text_src(lv_obj_t * obj, char * txt_src[]);
+void lv_scale_set_text_src(lv_obj_t * obj, const char * txt_src[]);
 
 /**
  * Draw the scale after all the children are drawn
